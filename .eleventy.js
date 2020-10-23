945f5cb0ad61a3cb5cf79c1f72f1d7e56f447137
Martin Hujer <mail@martinhujer.cz>
2020-10-23 23:16:01 +0200

    Blog

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,15 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
 
+    eleventyConfig.addFilter("dateformat", function(dateIn) {
+        const day = new Intl.DateTimeFormat('cs-CZ', { day: 'numeric' }).format(dateIn);
+        const month = new Intl.DateTimeFormat('cs-CZ', { month: 'numeric' }).format(dateIn);
+        const year = new Intl.DateTimeFormat('cs-CZ', { year: 'numeric' }).format(dateIn);
+        //console.log(`${day}. ${month}. ${year}`);
+
+        return `${day}. ${month}. ${year}`;
+    });
+
     return {
         dir: {
             input: "web",
