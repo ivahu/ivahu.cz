6c3f66e387a050acbb9b1209b0a2498d736b1f09
Martin Hujer <mail@martinhujer.cz>
2020-10-23 23:43:00 +0200

    Blog: display recent posts in footer (except on /blog/)

--- .eleventy.js
+++ .eleventy.js
@@ -10,6 +10,10 @@ module.exports = function(eleventyConfig) {
         return `${day}. ${month}. ${year}`;
     });
 
+    eleventyConfig.addFilter('slicejs', function (dataIn) {
+        return dataIn.slice(0, 3);
+    });
+
     return {
         dir: {
             input: "web",
