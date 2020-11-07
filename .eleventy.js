bb14b34c3acc449e721c89b3bd62fe4a3dffcfbe
Martin Hujer <mail@martinhujer.cz>
2020-11-07 17:57:49 +0100

    Redesign

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,19 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
 
+    eleventyConfig.addFilter("svgContents", function(file) {
+        const fs = require('fs');
+
+        let relativeFilePath = `.${file}`
+
+        let data = fs.readFileSync(relativeFilePath, function(err, contents) {
+            if (err) return err
+            return contents
+        });
+
+        return data.toString('utf8')
+    });
+
     return {
         dir: {
             input: "web",
